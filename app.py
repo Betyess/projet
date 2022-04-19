79cec502c81af8c910303a70b64949ad0bc513f7
farah essaidi <frhessaidi@gmail.com>
2022-04-20 01:48:26 +0200

    mise à jour

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def ajouter():
     remarque = entrerremarque.get()
 
     #Creeon la connexion
-    con = sqlite3.connect('hopital.db')
+    con = sqlite3.connect('participants.db')
     cuser = con.cursor()
     cuser.execute("insert into patient('code','nom','prenom','age','adresse','telephone','remarque') values (?,?,?,?,?,?,?)",(matricule,nom,prenom,age,adresse,telephone,remarque))
     con.commit()
@@ -22,7 +22,7 @@ def ajouter():
     messagebox.showinfo("Patient ajouter")
 
     #afficher
-    con = sqlite3.connect('hopital.db')
+    con = sqlite3.connect('participants.db')
     cuser = con.cursor()
     select = cuser.execute("select *from patient order by code desc")
     select = list(select)
@@ -39,7 +39,7 @@ def modifier():
         remarque = entrerremarque.get()
 
         # Creeon la connexion
-        con = sqlite3.connect('hopital.db')
+        con = sqlite3.connect('participants.db')
         cuser = con.cursor()
         cuser.execute(
             "update patient set nom=?,prenom=?,age=?,adresse=?,telephone=?,remarque=? where code=?",
@@ -49,7 +49,7 @@ def modifier():
         messagebox.showinfo("Patient Modifier")
 
         # afficher
-        con = sqlite3.connect('hopital.db')
+        con = sqlite3.connect('participants.db')
         cuser = con.cursor()
         select = cuser.execute("select * from patient order by code desc")
         select = list(select)
@@ -59,7 +59,7 @@ def modifier():
 
 def supprimer():
     codeSelectionner = table.item(table.selection())['values'][0]
-    con = sqlite3.connect("hopital.db")
+    con = sqlite3.connect("participants.db")
     cuser = con.cursor()
     delete  =cuser.execute("delete from patient where code = {}".format(codeSelectionner))
     con.commit()
@@ -69,34 +69,34 @@ def supprimer():
 
 #titre general
 root = Tk()
-root.title("Gestion des patient ")
+root.title("Gestion des participants ")
 root.geometry("1300x700")
 
 
 #Ajouter le titre
-lbltitre = Label(root,bd = 20, relief = RIDGE, text = "GESTION DES PATIENTS CHEZ JBDEVHOPITAL95", font = ("Arial", 30), bg = "darkblue", fg="white")
+lbltitre = Label(root,bd = 20, relief = RIDGE, text = "GESTION DES PARTICIPANTS AU CONCOURS de FOOT", font = ("Arial", 30), bg = "darkblue", fg="white")
 lbltitre.place(x = 0, y = 0, width = 1365)
 
 #Liste des patients
-lblListePatient = Label(root, text = "LISTES DES PATIENTS ", font = ("Arial", 16), bg = "darkblue", fg="white")
+lblListePatient = Label(root, text = "LISTES DES PARTICIPANTS ", font = ("Arial", 16), bg = "darkblue", fg="white")
 lblListePatient.place(x=600,y=100,width=760)
 
 
 
 #text matricule
-lblmatricule = Label(root, text = "Matricule Patient", font = ("Arial", 16), bg = "black", fg="white")
+lblmatricule = Label(root, text = "Matricule Participant", font = ("Arial", 16), bg = "black", fg="white")
 lblmatricule.place(x=0,y=100,width=200)
 entrermatricule = Entry(root)
 entrermatricule.place(x=200,y=100,width=160,height=30)
 
 #text nom
-lblnom = Label(root, text = "Nom Patient", font = ("Arial", 16), bg = "black", fg="white")
+lblnom = Label(root, text = "Nom Participant", font = ("Arial", 16), bg = "black", fg="white")
 lblnom.place(x=0,y=150,width=200)
 entrernom = Entry(root)
 entrernom.place(x=200,y=150,width=200,height=30)
 
 #text prenom
-lblPrenom = Label(root, text = "Prenom Patient", font = ("Arial", 16), bg = "black", fg="white")
+lblPrenom = Label(root, text = "Prenom Participant", font = ("Arial", 16), bg = "black", fg="white")
 lblPrenom.place(x=0,y=200,width=200)
 entrerPrenom = Entry(root)
 entrerPrenom.place(x=200,y=200,width=200,height=30)
@@ -108,19 +108,19 @@ entrerage = Entry(root)
 entrerage.place(x=200,y=250,width=100,height=30)
 
 #text adresse
-lbladresse = Label(root, text = "Adresse Patient", font = ("Arial", 16), bg = "black", fg="white")
+lbladresse = Label(root, text = "Adresse Participant", font = ("Arial", 16), bg = "black", fg="white")
 lbladresse.place(x=0,y=300,width=200)
 entreradresse = Entry(root)
 entreradresse.place(x=200,y=300,width="300",height=30)
 
 #text Telephone
-lbltelephone = Label(root, text = "Telephone Patient", font = ("Arial", 16), bg = "black", fg="white")
+lbltelephone = Label(root, text = "Telephone Participant", font = ("Arial", 16), bg = "black", fg="white")
 lbltelephone.place(x=0,y=350,width=200)
 entrertelephone = Entry(root)
 entrertelephone.place(x=200,y=350,width=200,height=30)
 
 #text remarque
-lblremarque = Label(root, text = "Remarque Patient", font = ("Arial", 16), bg = "black", fg="white")
+lblremarque = Label(root, text = "Remarque Participant", font = ("Arial", 16), bg = "black", fg="white")
 lblremarque.place(x=0,y=400,width=200)
 entrerremarque = Entry(root)
 entrerremarque.place(x=200,y=400,width=300,height=30)
@@ -163,7 +163,7 @@ table.column(6,width = 100)
 table.column(7,width = 150)
 
 # afficher les informations de la table
-con =  sqlite3.connect('hopital.db')
+con =  sqlite3.connect('participants.db')
 cuser = con.cursor()
 select = cuser.execute("select * from patient")
 for row in select:
